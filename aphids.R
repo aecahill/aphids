28e8c3fc95b2aeb87397e61fbe1cec2f79899396
aecahill <aecsk8gr8@yahoo.com>
2020-04-12 20:41:14 -0400

    Update aphids.R

--- aphids.R
+++ aphids.R
@@ -5,7 +5,7 @@ library(pegas)
 
 #read in fasta
 
-aphids<- read.FASTA("C:/Users/acahill/Documents/Github/aphids/aphidsloc.fasta")
+aphids<- read.FASTA("C:/Users/aecsk/Desktop/aphids.fasta")
 
 #paired distance matrix
 dist.dna(aphids)
@@ -27,14 +27,14 @@ aphids_haplo
 aphidsNet <- haploNet(aphids_haplo)
 
 #plot the network
-plot(aphidsNet, size = attr(aphidsNet, "freq"), fast = FALSE)
+plot(aphidsNet, size = attr(aphidsNet, "freq"), fast = F, show.mutation=0)
 
 #now get colors on the piechart
-aphidhaps<-read.table("C:/Users/acahill/Documents/GitHub/aphids/aphidhaps.txt",header=TRUE)
+aphidhaps<-read.table("C:/Users/aecsk/Documents/GitHub/aphids/aphidhaps2020.txt",header=TRUE)
 aphidmatrix<-as.matrix(aphidhaps)
-aphidmatrix2<-aphidmatrix[,2:3]
+aphidmatrix2<-aphidmatrix[,2:5]
 
-plot(aphidsNet, size=attr(aphidsNet, "freq"),fast=FALSE, scale.ratio = 2, cex = 0.5, pie=aphidmatrix2)
+plot(aphidsNet, size=attr(aphidsNet, "freq"),fast=FALSE, show.mutation=0, scale.ratio = 0.25, cex = 0.5, pie=aphidmatrix2)
 
-legend("bottomleft", c("Pennsylvania","Michigan"), text.col=c("red","cyan"))
+legend("bottomleft", c("Pennsylvania","Albion","South Haven","Saut Ste Marie"), text.col=c("red","cyan","green","black"))
 
