3d207f8333422707718374ae494efa1d346c15f0
aecahill <aecsk8gr8@yahoo.com>
2020-11-28 17:48:09 -0500

    aphids_sicb_nov28
    First code for SICB talk figs

---
+++ sicb_aphids.R
@@ -0,0 +1,81 @@
+library(ggplot2)
+
+#read in data for fall only
+aphidsfall<-read.table("C:/Users/aecsk/OneDrive/Desktop/aphidsfall.txt",header=T)
+
+#calculate stats by region
+summary(aov(aphidsfall$AvgAphidsPerLeaf~aphidsfall$Region))
+
+#graph by region
+ggplot(aphidsfall, aes(color = Region,y=AvgAphidsPerLeaf,x=Region)) + 
+  geom_jitter(position=position_jitter(0.2),cex=3)+
+  geom_boxplot(alpha=0.5)+
+  scale_color_manual(values=c("gold","purple"))+
+  #stat_summary(fun.y=mean, geom="point", shape=18,size=5, color="black")+
+  #stat_summary(fun.data=data_summary, color="red", size=1.5)+
+  labs(x ="Region", y = "Aphid Density")+
+  theme_bw()+
+  theme(legend.position="none")+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank())+
+  theme(axis.text.x = element_text(angle = 65,hjust=1))
+
+#trying with fall only, no zeros
+
+aphidsfallnz<-read.table("C:/Users/aecsk/OneDrive/Desktop/aphidsfallnozero.txt",header=T)
+
+summary(aov(aphidsfallnz$AvgAphidsPerLeaf~aphidsfallnz$Region))
+
+ggplot(aphidsfallnz, aes(color = Region,y=AvgAphidsPerLeaf,x=Region)) + 
+  geom_jitter(position=position_jitter(0.2),cex=3)+
+  geom_boxplot(alpha=0.5)+
+  scale_color_manual(values=c("gold","purple"))+
+  #stat_summary(fun.y=mean, geom="point", shape=18,size=5, color="black")+
+  #stat_summary(fun.data=data_summary, color="red", size=1.5)+
+  labs(x ="Region", y = "Aphid Density")+
+  theme_bw()+
+  theme(legend.position="none")+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank())+
+  theme(axis.text.x = element_text(angle = 65,hjust=1))
+
+#plotting correlations with height and leaf number
+
+cor.test(aphidsfall$Height_cm,aphidsfall$AvgAphidsPerLeaf)
+
+#and with no zeroes
+
+cor.test(aphidsfallnz$Height_cm,aphidsfallnz$AvgAphidsPerLeaf)
+
+ggplot(aphidsfall,aes(Height_cm,AvgAphidsPerLeaf))+
+  geom_point(aes(color=Region), alpha = 0.65, size = 4)+
+  theme_bw()+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank())+
+  scale_colour_manual(values=c("gold","purple"))+
+  theme(axis.text.x= element_text(size=16))+
+  theme(axis.text.y= element_text(size=16))+
+  theme(axis.title.x=element_text(size=16))+
+  theme(axis.title.y=element_text(size=16))+
+  xlab("\nPlant Height (cm)")+ylab("Aphid Density\n")
+
+ggplot(aphidsfallnz,aes(Height_cm,AvgAphidsPerLeaf))+
+  geom_point(aes(color=Region), alpha = 0.65, size = 4)+
+  theme_bw()+
+  theme(panel.background = element_blank(), 
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        plot.background = element_blank())+
+  scale_colour_manual(values=c("gold","purple"))+
+  theme(axis.text.x= element_text(size=16))+
+  theme(axis.text.y= element_text(size=16))+
+  theme(axis.title.x=element_text(size=16))+
+  theme(axis.title.y=element_text(size=16))+
+  xlab("\nPlant Height (cm)")+ylab("Aphid Density\n")
+
